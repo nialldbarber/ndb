9f31ee8492f835cbab4f21a1107f6c8234b125e8
Niall Barber <niall.barber@actual-experience.com>
2021-01-19 10:48:30 +0000

    Somewhat sorted badge styles

--- src/components/badge.tsx
+++ src/components/badge.tsx
@@ -7,9 +7,14 @@ interface BadgeProps {
 
 const BadgeContainer = styled.span`
   margin: 0 0.3rem;
-  padding: 0.5rem;
-  border: 1px solid ${({theme}) => theme.colors.white};
+  padding: 0.3rem 0.5rem;
+  border: 1px solid ${({theme}) => theme.colors.pink};
   border-radius: 5px;
+
+  span {
+    color: ${({theme}) => theme.colors.pink};
+    font-size: 0.875rem;
+  }
 `;
 
 const Badge: FC<BadgeProps> = ({text}) => {
--- src/templates/all-posts.tsx
+++ src/templates/all-posts.tsx
@@ -80,12 +80,34 @@ const Grid = styled.div`
 `;
 
 const Card = styled.div`
-  display: flex;
-  flex-direction: column;
-  padding: 1rem;
+  display: grid;
+  grid-template-rows: 1fr 110px 1fr;
+  height: 250px;
+  padding: 1rem 1.5rem;
   background: ${({theme}) => theme.colors.darkerBlack};
   border-bottom-right-radius: 15px;
   color: ${({theme}) => theme.colors.white};
+  transform: scale(1);
+  transition: transform 0.2s ease;
+
+  &:hover {
+    transform: scale(1.03);
+  }
+
+  h3 {
+    margin: 0;
+    color: ${({theme}) => theme.colors.pink};
+    font-size: 1rem;
+  }
+
+  p {
+    margin: 0;
+    font-size: 1.3rem;
+  }
+`;
+
+const BadgeContainer = styled.div`
+  align-self: center;
 `;
 
 const AllPosts: FC<PostProps> = ({pageContext, data}) => {
@@ -120,11 +142,11 @@ const AllPosts: FC<PostProps> = ({pageContext, data}) => {
                 <Card key={slug}>
                   <h3>{title}</h3>
                   <p>{excerpt}</p>
-                  <div>
+                  <BadgeContainer>
                     {tags.map((tag, i) => (
                       <Badge key={i} text={tag} />
                     ))}
-                  </div>
+                  </BadgeContainer>
                 </Card>
               </Link>
             );

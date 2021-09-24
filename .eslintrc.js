6230e7d988efdb1e26c296cef49658815b9723b3
Niall Barber <niall.barber@actual-experience.com>
2021-09-24 15:54:06 +0100

    Updated eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,13 +4,16 @@ module.exports = {
     'prefer-const': 'error',
     'import/no-extraneous-dependencies': 0,
     'import/no-unresolved': 0,
-    'react/jsx-filename-extension': 0,
     'react/prop-types': 0,
     'arrow-body-style': 0,
     'no-use-before-define': 0,
     'lines-between-class-members': 0,
     'import/no-mutable-exports': 0,
     'no-nested-ternary': 0,
+    // react
+    'react/jsx-filename-extension': 0,
+    "react/jsx-uses-react": 0,
+    "react/react-in-jsx-scope": 0,
     // Typescript
     '@typescript-eslint/no-explicit-any': 0,
     '@typescript-eslint/no-unsafe-assignment': 0,

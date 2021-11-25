46fc37c010bb13d30ea5b03b79e81adc6d82dd8c
Niall <nialldbarber@gmail.com>
2021-11-25 08:31:51 +0000

    Added a lil 🐰🥚

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -2,6 +2,11 @@ import React from 'react';
 import {AnimatePresence} from 'framer-motion';
 import MainLayout from './src/components/layouts/main';
 
+console.log(
+  '%c+',
+  'font-size: 1px; padding: 80px 150px; line-height: 50px; background: url(https://c.tenor.com/Ry-jYCyu2O8AAAAd/mega64-hacking-in-progress.gif); background-size: 400px 250px; background-repeat: no-repeat; color: transparent;'
+);
+
 const transitionDelay = 500;
 
 export const wrapPageElement = ({element, props}) => {
